871eed69767fdf639adccc5e4e8ada0083996649
Bruno Pereira <brunoalmeidapereira@gmail.com>
2024-11-13 07:46:33 -0300

    Feat: add function to toogle task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,34 +1,50 @@
-import { Check, Trash } from "phosphor-react"
-import styles from './Task.module.css'
-import { TaskInterface } from "../App"
+import { Check, Trash } from "phosphor-react";
+import styles from './Task.module.css';
+import { TaskInterface } from "../App";
 
-interface props {
-    data: TaskInterface
+interface Props {
+    data: TaskInterface;
+    removeTask: (id: number) => void
+    toggleTaskStatus: ({ id, value }: { id: number; value: boolean }) => void
 }
 
-function Task(props:props) {
+function Task(props: Props) {
 
-    return (
+    function handleTaskToggle() {
+       props.toggleTaskStatus({ id: props.data.id, value: !props.data.isChecked })
+      }
+    
+      function handleRemove() {
+       props.removeTask(props.data.id)
+      }
+    
+      const checkboxCheckedClassname =props.data.isChecked
+        ? styles['checkbox-checked']
+        : styles['checkbox-unchecked']
+      const paragraphCheckedClassname = props.data.isChecked
+        ? styles['paragraph-checked']
+        : ''
+    
+      return (
         <div className={styles.container}>
-            <div>
-                <label htmlFor="checkbox" >
-                    <input type="checkbox" />
-                    <span className={`${styles.checkbox} `}>
-
-                        <Check size={12} />
-                    </span>
-
-                    <p className={`${styles.paragraph} `}>
-                       {props.data.text}
-                    </p>
-                </label>
-            </div>
-
-            <button>
-                <Trash size={16} color="#808080" />
-            </button>
+        
+            <label htmlFor="checkbox" onClick={handleTaskToggle} >
+              <input readOnly type="checkbox" checked={props.data.isChecked}   />
+              <span className={`${styles.checkbox} ${checkboxCheckedClassname}`}>
+                {props.data.isChecked && <Check size={12} />}
+              </span>
+    
+              <p className={`${styles.paragraph} ${paragraphCheckedClassname} `} >
+                {props.data.text}
+              </p>
+            </label>
+    
+    
+          <button onClick={handleRemove}>
+            <Trash size={16} color="#808080" />
+          </button>
         </div>
-    )
+      )
+}
 
-
-} export default Task
+export default Task;
